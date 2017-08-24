08b8c49701fba42aa90df36f9280696298cb255c
c2huc2hu <c2huc2hu@gmail.com>
2017-08-23 21:44:31 -0400

    initial commit: part 1, encrypt/ decryption

---
+++ part1.js
@@ -0,0 +1,83 @@
+// started at 11:50
+// finished at 12:50
+
+// All of these functions take an uppercase string and key, and return a string.
+// Ignore non-uppercase letters
+
+const _ = require('lodash');
+
+function is_uppercase(ch) {
+    let cc = ch.charCodeAt(0);
+    return 65 <= cc && cc <= 90;
+}
+
+function caesar_encrypt(message, key) {
+    let ords = _.map(message, ch => {
+        if(!is_uppercase(ch)) {
+            return ch.charCodeAt(0);
+        }
+        return (ch.charCodeAt(0) - 65 + key + 26) % 26 + 65;
+    });
+    return String.fromCharCode(...ords);
+}
+
+function caesar_decrypt(cipher, key) {
+    return caesar_encrypt(cipher, -key);
+}
+
+function vigenere_encrypt(message, key) {
+    let ords = _.map(message, (ch, index) => {
+        if(!is_uppercase(ch)) {
+            return ch.charCodeAt(0);
+        }
+        return (ch.charCodeAt(0) - 65 + key[index % key.length] + 26) % 26 + 65;
+    });
+    return String.fromCharCode(...ords)
+}
+
+function vigenere_decrypt(cipher, key) {
+    let inverse_key = key.map(x => -x);
+    return vigenere_encrypt(cipher, inverse_key)
+}
+
+ // flagging potential issue: sending weird unprintable characters in html body. Also be careful about null terminated strings
+function xor_decrypt(message, key) {
+    let ords = _.map(message, (ch, index) => {
+        return ch.charCodeAt(0) ^ key[index % key.length];
+    });
+    return String.fromCharCode(...ords)
+}
+
+function xor_encrypt(cipher, key) {
+    return xor_decrypt(cipher, key);
+}
+
+// key is a permutation of the alphabet
+function poly_decrypt(message, key) {
+    // construct the object
+    let encrypt_table = {};
+    for(let i=0; i<26; i++) {
+        encrypt_table[key.charCodeAt(i)] = i + 65;
+    }
+
+    let result_array = _.map(cipher, ch => {
+        return encrypt_table[ch] ? encrypt_table[ch] : ch;
+    });
+    return String.fromCharCode(...result_array);
+}
+
+function poly_encrypt(cipher, key) {
+    let encrypt_table = {};
+    for(let i=0; i<26; i++) {
+        encrypt_table[String.fromCharCode(i + 65)] = key.charCodeAt(i);
+    }
+
+    let result_array = _.map(cipher, ch => {
+        return encrypt_table[ch];
+    });
+    return String.fromCharCode(...result_array);
+}
+
+// console.log(caesar_decrypt(caesar_encrypt("HELLO WORLD!", 3), 3));
+console.log(vigenere_encrypt("HELLO WORLD!", [1, 4, 2]));
+console.log(vigenere_decrypt(vigenere_encrypt("HELLO WORLD!", [1, 4, 2]), [1, 4, 2]));
